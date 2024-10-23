cc7d82bf0da09dca958489ce632fbe476243735e
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-10-23 13:48:44 +0900

    CLAB-8 feat: spring initializr

---
+++ src/main/java/com/cpplab/CpplabApplication.java
@@ -0,0 +1,13 @@
+package com.cpplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpplabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpplabApplication.class, args);
+	}
+
+}
