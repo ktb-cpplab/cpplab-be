ab7d47007b3e1ca3f5afb1ec1ecfe705fb079c16
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-10-28 20:00:01 +0900

    CLAB-48 feat: 게시판 생성 제작

--- src/main/java/com/cpplab/domain/auth/entity/UserEntity.java
+++ src/main/java/com/cpplab/domain/auth/entity/UserEntity.java
@@ -14,7 +14,7 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long user_id;
 
     private String username;
     private String name;
---
+++ src/main/java/com/cpplab/domain/community/entity/PostEntity.java
@@ -0,0 +1,27 @@
+package com.cpplab.domain.community.entity;
+
+import com.cpplab.domain.auth.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long post_id;
+
+    private String title; // 제목
+    private String content; // 내용
+    private Long views; // 조회수
+    private Long likes; // 조회수
+
+
+    @ManyToOne // // 여러 개의 PostEntity가 하나의 UserEntity
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+}
