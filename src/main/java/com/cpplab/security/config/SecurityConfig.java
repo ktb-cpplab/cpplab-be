b51a56a987b37b6ca303f28cac2e073da0d00e19
kakao-tech-bootcamp <theka0304@gmail.com>
2024-10-28 17:59:35 +0900

    Fix: db, health check

--- src/main/java/com/cpplab/security/config/SecurityConfig.java
+++ src/main/java/com/cpplab/security/config/SecurityConfig.java
@@ -1,28 +1,29 @@
 package com.cpplab.security.config;
 
 
-import com.cpplab.domain.auth.repository.RefreshRepository;
-import com.cpplab.domain.auth.service.CustomOAuth2UserService;
-import com.cpplab.security.handler.CustomSuccessHandler;
-import com.cpplab.security.jwt.CustomLogoutFilter;
-import com.cpplab.security.jwt.JWTFilter;
-import com.cpplab.security.jwt.JWTUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Collections;
+import com.cpplab.domain.auth.repository.RefreshRepository;
+import com.cpplab.domain.auth.service.CustomOAuth2UserService;
+import com.cpplab.security.handler.CustomSuccessHandler;
+import com.cpplab.security.jwt.CustomLogoutFilter;
+import com.cpplab.security.jwt.JWTFilter;
+import com.cpplab.security.jwt.JWTUtil;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -85,6 +86,7 @@ public class SecurityConfig {
     }
 
     private static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/**",
             "/",
             "/reissue", // 엑세스토큰 리프레시 토큰으로 재발급 경로
             "/access", // 첫 로그인시, 엑세스 토큰 헤더 전달을 위한 경로
