fb0e4909803d5a8bce56f25c72e107ed2a0573d8
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-10-30 13:46:53 +0900

    CLAB-67 refact: 공통응답 RestControllerAdvice로 변경

--- src/main/java/com/cpplab/global/common/code/BaseCode.java
+++
@@ -1,6 +0,0 @@
-package com.cpplab.global.common.code;
-
-public interface BaseCode {
-    public ReasonDTO getReason();
-    public ReasonDTO getReasonHttpStatus();
-}
--- src/main/java/com/cpplab/global/common/code/BaseErrorCode.java
+++
@@ -1,6 +0,0 @@
-package com.cpplab.global.common.code;
-
-public interface BaseErrorCode {
-    public ErrorReasonDTO getReason();
-    public ErrorReasonDTO getReasonHttpStatus();
-}
