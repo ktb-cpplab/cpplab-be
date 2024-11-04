fe164c8ec773cd9afa8c6f3aaebfbb1785bcb70c
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-11-04 15:41:13 +0900

    CLAB-81 feat: 로드맵 entity 구조 수정

--- src/main/java/com/cpplab/domain/roadmap/controller/RoadmapController.java
+++ src/main/java/com/cpplab/domain/roadmap/controller/RoadmapController.java
@@ -1,10 +1,23 @@
 package com.cpplab.domain.roadmap.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/roadmap")
 public class RoadmapController {
 
+
+    // 로드맵 저장
+    @PostMapping("")
+
+
+
+// 로드맵 조회
+//    @GetMapping("")
+// 로드맵 스탭에서 체크
+//    @PatchMapping("/{projectId}/{stepId}")
 
 
 }
--- src/main/java/com/cpplab/domain/roadmap/entity/TaskEntity.java
+++ src/main/java/com/cpplab/domain/roadmap/entity/TaskEntity.java
@@ -1,20 +1,19 @@
 package com.cpplab.domain.roadmap.entity;
 
-import com.cpplab.domain.auth.entity.UserEntity;
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
-public class StepEntity {
+public class TaskEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long stepId;
+    private Long taskId;
 
-    private String stepTitle;
+    private String stepTitle; // 업무제목
+    private boolean isCompleted; // 완료 여부
 
     @ManyToOne // // 여러 개의 StepEntity 하나의 RoadmapEntity
     @JoinColumn(name = "roadmapId")
-    private RoadmapEntity roadmap;
+    private StepEntity step;
+
 }
