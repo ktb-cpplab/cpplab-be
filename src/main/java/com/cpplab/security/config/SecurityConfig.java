7152491f6acd17ad7d683593f891935c9261eef3
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-11-08 01:02:14 +0900

    CLAB-91 feat: 쿠키허용 추가

--- src/main/java/com/cpplab/security/config/SecurityConfig.java
+++ src/main/java/com/cpplab/security/config/SecurityConfig.java
@@ -54,7 +54,10 @@ public class SecurityConfig {
                 configuration.setAllowCredentials(true);
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
                 configuration.setMaxAge(3600L);
+
+                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
                 configuration.setExposedHeaders(Collections.singletonList("access"));
+
                 return configuration;
             }
         }));
