5abe9cfaa4e22bbc3383d494d3b4545bddc58d65
s1lv3rrud <lek8575@naver.com>
2024-11-11 22:14:23 +0900

    chore: 리다이렉트 비활성화

--- src/main/java/com/cpplab/global/config/RestTeplateConfig.java
+++ src/main/java/com/cpplab/global/config/RestTeplateConfig.java
@@ -1,7 +1,10 @@
 package com.cpplab.global.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,7 +12,12 @@ public class RestTeplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+        // Apache HttpClient 생성 및 리다이렉트 비활성화
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .disableRedirectHandling()  // 리다이렉트 방지 설정
+                .build();
 
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
+    }
 }
