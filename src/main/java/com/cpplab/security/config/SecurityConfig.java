cdab79e43192a312b57ce21fd57368ff28ad61f8
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-11-12 00:34:40 +0900

    fix: secure제거 및 securityConfig set-cookie 제거

--- src/main/java/com/cpplab/security/config/SecurityConfig.java
+++ src/main/java/com/cpplab/security/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
                 configuration.setMaxAge(3600L);
 
-                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+//                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
                 configuration.setExposedHeaders(Collections.singletonList("access"));
 
                 return configuration;
