416e13f45a1dc44407826924b18e4d0e633d61c3
leeyongwoo <95459741+softwareyong@users.noreply.github.com>
2024-12-04 21:58:39 +0900

    CLAB-114 chore: 리팩토링

--- src/main/java/com/cpplab/CpplabApplication.java
+++ src/main/java/com/cpplab/CpplabApplication.java
@@ -1,8 +1,10 @@
 package com.cpplab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@SpringBootApplication
@@ -13,5 +15,11 @@ public class CpplabApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CpplabApplication.class, args);
 	}
+
+	// HTTP 요청과 응답 과거 기록을 확인
+//	@Bean
+//	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+//		return new InMemoryHttpExchangeRepository();
+//	}
 
 }
